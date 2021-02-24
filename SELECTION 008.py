710ef338e3b07091c522ad254242ac8095b93418
SamuelCoelhoOPHS <71763353+SamuelCoelhoOPHS@users.noreply.github.com>
2021-02-24 10:58:58 +0000

    Add files via upload

---
+++ SELECTION 008.py
@@ -0,0 +1,13 @@
+num = int(input("Enter a number 1, 2, 3 >>> "))
+
+if num == 1:
+    print("thank you")
+
+elif num == 2:
+    print("well done, you're so smart")
+
+elif num == 3:
+    print("amazing, you are so good at this")
+
+else:
+    print("really? What is wrong with you??")
